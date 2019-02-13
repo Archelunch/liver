f133f9697f7c99bae35aaef21b5f4b278fb93387
Michael Pavlukhin <alivemihail@gmail.com>
2019-02-13 17:44:04 +0300

    fixxx

--- online_test/asgi.py
+++ online_test/asgi.py
@@ -8,9 +8,6 @@ import django
 from channels.routing import get_default_application
 from whitenoise import WhiteNoise
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "online_test.settings")
 django.setup()
 application = get_default_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, "static"))
