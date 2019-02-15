3912efabcf76a4789bca86f571e978bdd635aaa5
Michael Pavlukhin <alivemihail@gmail.com>
2019-02-15 11:12:16 +0300

    fa

--- liver/views.py
+++ liver/views.py
@@ -103,7 +103,6 @@ def voter(request, url):
         "is_started" : quiz.is_started,
         "range": range(len([str(ans) for ans in question.get_answers()]))
         }
-        print(data['answers'])
         return render(request, 'voterka.html', data)
     return render(request, '404.html')
 
