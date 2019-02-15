95702ff963d6b9818aa4301a9fd4808bffc8f349
Michael Pavlukhin <alivemihail@gmail.com>
2019-02-15 10:00:52 +0300

    Afd

--- liver/urls.py
+++ liver/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('leaderboard', views.leaderborad, name='leaderboard'),
     path('api/vote', views.vote, name="api.vote"),
     path('<url>', views.voter, name='vote_room'),
-    path('<url>/<code>', views.quiz_start, name='quiz_room'),
     path('<url>/v_<code>', views.voter_panel, name='vote_panel'),
+    path('<url>/<code>', views.quiz_start, name='quiz_room'),
 ]
