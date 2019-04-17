a0789cb2d6a96f1b045715c76e76f127237a905c
Michael Pavlukhin <alivemihail@gmail.com>
2019-04-17 12:35:11 +0300

    something with question list

---
+++ adminka/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from quiz.models import Quiz, Category, Progress, Sitting, Question, QUser, QuizProgress
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        if request.POST.get("passwordInput", "") == "mehpassword" and request.POST.get("loginInput", "") == "mehadmin":
+            request.session['login'] = "mehadmin"
+            redirect('quiz_list')
+    return render(request, 'master-login.html')
+
+
+def quiz_list(request):
--- online_test/settings.py
+++ online_test/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'mcq',
     'quiz',
     'liver',
+    'adminka',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
