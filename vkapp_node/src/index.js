be2e5001bd91f8f06214a6abf121b0e9d4f78484
Arseniy Beresnev <arseniy.beresnev@gmail.com>
2019-04-27 20:20:48 +0500

    omitted app and panel

--- vkapp_node/src/index.js
+++ vkapp_node/src/index.js
@@ -6,6 +6,16 @@ import connect from '@vkontakte/vkui-connect';
 connect.send('VKWebAppInit', {});
 
 ReactDOM.render(
-    <App />,
+    <div style={{width: "100vw", height: "100vh"}}>
+                <div id="gamezone">
+                    <div className="center" id="main">
+                        <button className="btn btn-main center" id="mainButton">HANDSAPP</button>
+                    </div>
+                </div>
+                <div id="footemail">
+                    <a href="mailto:info@handsapp.fun" target="_top" className="footer">info@handsapp.fun</a>
+                </div>
+                <h2 id="timer"></h2>
+            </div>,
     document.getElementById('root')
 );
