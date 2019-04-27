b4600345e08e578ae519a077508629bbba76a92e
Arseniy Beresnev <arseniy.beresnev@gmail.com>
2019-04-27 19:54:03 +0500

    fix #idunno

--- vkapp_node/src/App.js
+++ vkapp_node/src/App.js
@@ -1,20 +1,10 @@
 import React, {Component} from 'react';
-import * as UI from '@vkontakte/vkui';
-import {isWebView} from '@vkontakte/vkui/src/lib/webview';
 import MainScreen from './MainScreen';
 
 class App extends Component {
     render() {
-        let activePanel = 'mainScreen';
-
         return (
-            <UI.ConfigProvider insets={this.props.insets} isWebView={isWebView}>
-                <UI.Root activeView="mainView">
-                    <UI.View id="mainView" activePanel={activePanel}>
-                        <MainScreen id="mainScreen" />
-                    </UI.View>
-                </UI.Root>
-            </UI.ConfigProvider>
+            <MainScreen id="mainScreen" />
         );
     }
 }
