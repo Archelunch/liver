7c2f36caa4541c8aa6ba8822fe9578fa98a1851d
Arseniy Beresnev <arseniy.beresnev@gmail.com>
2019-04-29 20:57:52 +0500

    removed everything except vkapp

--- adminka/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminkaConfig(AppConfig):
-    name = 'adminka'
--- liver/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LiverConfig(AppConfig):
-    name = 'liver'
