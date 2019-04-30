ecee57bf5a3a5290592404db4d84fdd095d46700
Arseniy Beresnev <arseniy.beresnev@gmail.com>
2019-04-30 15:01:33 +0500

    sending data from admin panel to backend

--- static_temp/app.js
+++ static_temp/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let questionNumber = 1;
+
 function changeToTextArea(elementId, is_answer=false) {
 	let element = document.getElementById(elementId);
 	element.onclick = "";
@@ -8,8 +10,58 @@ function changeToTextArea(elementId, is_answer=false) {
 		element.innerHTML = "<textarea>Текст вопроса</textarea>";
 	} else {
 		element.style.borderRadius = "5vh";
-		element.innerHTML = "<textarea>Текст ответа</textarea><button class='removeAnswer' onclick='changeToPrompt(this.parentNode.id);'></button>";
+		element.innerHTML = "<textarea>Текст ответа</textarea><button class='markAsCorrect' onclick='markAsCorrect(this.parentNode.id);'></button>";
 	}
+}
+
+function markAsCorrect(elementId) {
+	let element = document.getElementById(elementId);
+	element.correct = "true";
+}
+
+function markAsIncorrect(elementId) {
+	let element = document.getElementById(elementId);
+	element.correct = "false";
+}
+
+function sendAnswer() {
+	let question = document.getElementById("question");
+	let answer1 = document.getElementById("answer1");
+	let answer2 = document.getElementById("answer2");
+	let answer3 = document.getElementById("answer3");
+	if (question.firstChild.tagName == "p") {
+		return false;
+	} else if (answer1.firstChild.tagName == "p") {
+		return false;
+	} else if (answer2.firstChild.tagName == "p") {
+		return false;
+	} else if (answer3.firstChild.tagName == "p") {
+		return false;
+	}
+	let data = {
+		id: document.getElementById("testId").innerHTML,
+		question: question.firstChild.innerHTML,
+		answers: {
+			[answer1.firstChild.innerHTML]: answer1.correct === "true" ? true : false,
+			[answer2.firstChild.innerHTML]: answer2.correct === "true" ? true : false,
+			[answer3.firstChild.innerHTML]: answer3.correct === "true" ? true : false
+		}
+	};
+	$.ajax("/", {
+		type: "post",
+		data: data,
+		success: (data) => {
+			changeToPrompt("question");
+			changeToPrompt("answer1");
+			changeToPrompt("answer2");
+			changeToPrompt("answer3");
+			markAsIncorrect("answer1");
+			markAsIncorrect("answer2");
+			markAsIncorrect("answer3");
+			questionNumber += 1;
+			document.getElementsByClassName("editor-header")[0].innerHTML = `${questionNumber}/...`;
+		}
+	});
 }
 
 function changeToPrompt(elementId) {
@@ -24,7 +76,7 @@ function changeToPrompt(elementId) {
 
 function addTest() {
 	let element = document.getElementsByClassName("row")[0];
-	let html = `<div style="height: 100vh; width: 100vw;">
+	let html = `<div style="height: 100vh; width: 100vw;" id="nameOverlay">
 		<div class="center">
 			<div class="input-holder"><input type="text" id="nameInput" name="nameInput" placeholder="Название" required/><button type="button" id="testButton"></button></div>
 		</div>
@@ -35,12 +87,17 @@ function addTest() {
 
 function addTestModule() {
 	$.ajax("/createQuiz", {
-		type: "GET",
+		type: "post",
 		data: {
 			title: document.getElementById("testButton").value;
 		},
 		success: (data) => {
+			document.getElementById("nameOverlay").outerHTML = "";
 			window.location.href = `/adminka/${data.id}`;
 		}
 	})
+}
+
+function goToAdminka() {
+	window.location.href = "/adminka";
 }
