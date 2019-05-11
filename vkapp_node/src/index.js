8ce56fa169f9c6bf177da251df65be562b4f2fcb
Arseniy Beresnev <arseniy.beresnev@gmail.com>
2019-05-11 21:02:56 +0500

    getting data from vk

--- vkapp_node/src/index.js
+++ vkapp_node/src/index.js
@@ -6,25 +6,25 @@ connect.send('VKWebAppInit', {});
 connect.subscribe((e) => {
     switch (e.detail.type) {
         case 'VKWebAppGetUserInfoResult':
-            console.log(e.detail.data);
+            console.log(e.detail.data.id);
+            ReactDOM.render(
+                <span className="d-none" id="vkid">e.detail.data.id</span>
+                <div style={{width: "100vw", height: "100vh"}}>
+                    <div id="gamezone">
+                        <div className="center" id="main">
+                            <button className="btn btn-main center" id="mainButton">ADD VK</button>
+                        </div>
+                    </div>
+                    <div id="footemail">
+                        <a href="https://vk.com/hands_app" target="_top" className="footer">Сделано в HandsApp</a>
+                    </div>
+                    <h2 id="timer"></h2>
+                </div>,
+                document.getElementById('root')
+            );
             break;
         default:
             console.log(e.detail.type);
     }
 });
 connect.send('VKWebAppGetUserInfo', {});
-
-ReactDOM.render(
-    <div style={{width: "100vw", height: "100vh"}}>
-                <div id="gamezone">
-                    <div className="center" id="main">
-                        <button className="btn btn-main center" id="mainButton">ADD VK</button>
-                    </div>
-                </div>
-                <div id="footemail">
-                    <a href="https://vk.com/hands_app" target="_top" className="footer">Сделано в HandsApp</a>
-                </div>
-                <h2 id="timer"></h2>
-            </div>,
-    document.getElementById('root')
-);
