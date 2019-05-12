6f06c220dcdbb5953ad3f80473945509c6d6b008
Arseniy Beresnev <arseniy.beresnev@gmail.com>
2019-05-12 23:49:25 +0500

    question fix, timer fix

--- vkapp_node/public/app.js
+++ vkapp_node/public/app.js
@@ -40,6 +40,9 @@ function addTest() {
 function convertTimeToNiceString(time) {
 	let minutes = Math.floor(time / 60);
 	let seconds = time - (minutes * 60);
+	if (seconds < 10) {
+		seconds = "0" + seconds.toString();
+	}
 	return `${minutes}:${seconds}`;
 }
 
