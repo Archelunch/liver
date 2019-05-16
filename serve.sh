1550148d244b1337de9541f929e24f7133ad028b
Arseniy Beresnev <arseniy.beresnev@gmail.com>
2019-05-16 07:27:02 +0500

    fixed text size, updated serve script

--- serve.sh
+++ serve.sh
@@ -1,3 +1,3 @@
 git pull
 python3.6 manage.py collectstatic --no-input
-daphne -e ssl:443:privateKey=/etc/letsencrypt/live/handsapp.fun/privkey.pem:certKey=/etc/letsencrypt/live/handsapp.fun/fullchain.pem online_test.asgi:application
+sudo daphne -e ssl:443:privateKey=/etc/letsencrypt/live/handsapp.fun/privkey.pem:certKey=/etc/letsencrypt/live/handsapp.fun/fullchain.pem online_test.asgi:application
