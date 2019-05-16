257e875e12533ea6fbe04fcdde0415dc4c6c7607
Arseniy Beresnev <arseniy.beresnev@gmail.com>
2019-05-16 07:29:16 +0500

    updated serve script, removed unnecessary file

--- vkapp_node/serve.sh
+++ vkapp_node/serve.sh
@@ -1,3 +1,3 @@
 git pull origin vkapp
 npm run build
-webpack-dev-server --https --cert=/etc/letsencrypt/live/handsapp.fun/fullchain.pem --key=/etc/letsencrypt/live/handsapp.fun/privkey.pem --host handsapp.fun --port 3000 --content-base build b
+sudo webpack-dev-server --https --cert=/etc/letsencrypt/live/handsapp.fun/fullchain.pem --key=/etc/letsencrypt/live/handsapp.fun/privkey.pem --host handsapp.fun --port 3000 --content-base build b
