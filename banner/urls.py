9755b0cc40993bcb14833fcfe5941d0b326c53c1
Михаил Павлухин <mike.pavlukhin@MacBook-Pro-Mihail.local>
2019-06-28 17:26:55 +0300

    fix

--- banner/urls.py
+++ banner/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('watcher', views.main_screen, name='mehikowatcher'),
+    path('check_people', views.check_people, name='checkpeople'),
     path('', views.voted, name='mehikovoter'),
 ]
